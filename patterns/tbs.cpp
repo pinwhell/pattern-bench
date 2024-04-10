8cdc59107dd61f2b33bcf73aae4b08e6c885b08f
Pinwhell <60289470+pinwhell@users.noreply.github.com>
2024-04-10 02:28:10 -0400

    TBS Update #3

--- patterns/tbs.cpp
+++ patterns/tbs.cpp
@@ -8,32 +8,6 @@
 
 using namespace TBS;
 
-std::string PatternStringFromPatternMask(const byte* pattern, const char* mask)
-{
-    auto len = strlen(mask);
-    std::string result;
-                                // 3 Caracters per byte maximum
-                                // 'XX ' ...
-    result.reserve(len * 3);    //  123  ...
-
-    for (size_t i = 0; i < len; i++)
-    {
-        if (mask[i] != '?')
-        {
-            char _byte[3] {0};
-            snprintf(_byte, sizeof(_byte), "%02X", pattern[i]);
-            result += std::string(_byte);
-        }
-        else
-            result += '?';
-
-        if (i + 1 < len)
-            result += ' ';
-    }
-
-    return result;
-}
-
 void TBSCacheFlush(State& state)
 {
     state.mSharedDescriptions.clear();
@@ -51,12 +25,12 @@ struct TBSScanner : pattern_scanner
         const byte* pattern, const char* mask, const byte* data, size_t length) const override
     {
         TBSCacheFlush(const_cast<State&>(state));
-        std::string fullPattern = PatternStringFromPatternMask(pattern, mask);
 
         Pattern::Description patternDesc = 
             templateBuilder
             .Clone()
-            .setPattern(fullPattern)
+            .setPatternRaw(pattern)
+            .setMask(mask)
             .setScanStart(data)
             .setScanEnd(data + length)
             .Build();
